4e838245fa19b5959131ba53e29c797b2a61d133
Levy Matheus <levymatheus2025@gmail.com>
2024-07-13 01:25:29 -0400

    page maintance

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,17 +12,17 @@ const MaintenancePage = () => {
         <meta property="og:title" content="Site em Manutenção" key="title" />
         <meta name="description" content="Estamos trabalhando para melhorar nosso site. Voltaremos em breve!" />
       </Head>
-      <main>
-       <div className={styles.maintenanceContainer}>
-       <h1>Estamos em Manutenção</h1>
-       <p>Desculpe pelo transtorno. Estamos trabalhando para melhorar nosso site e voltaremos em breve!</p>
-       </div>
+      <main className={styles.maintenanceContainer}>
+        <h1>Estamos em Manutenção</h1>
+        <p>Desculpe pelo transtorno. Estamos trabalhando para melhorar nosso site e voltaremos em breve!</p>
         <div className={styles.maintenanceImage}>
           <img src="/web-maintenance.png"alt="Manutenção" />
         </div>
         <div className={styles.maintenanceImage}>
         <img src="/gamersNews.png" alt="Logo" />   
         </div>
+        <p>Entre em contado com os desenvolvedores em caso de dúvida:</p>
+        <p>https://www.instagram.com/levymattheeus/ ou https://www.instagram.com/djandredantas/</p>
       </main>
     </>
   );
